c1f85637880db9df7f412005631d84136db7729f
[Ayoub] <[ayoublzr93@gmail.com]>
2023-05-08 22:22:17 +0200

    add_filter_in_product_page

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,52 +1,103 @@
 import "bootstrap/dist/css/bootstrap.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/navBar/Navbar";
 import Footer from "../../components/Footer/Footer";
-import axios from 'axios';
+import axios from "axios";
 import "./styleCard.css";
 import { Form } from "react-bootstrap";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products')
-      .then(res => {
+    axios
+      .get("http://localhost:3000/api/products")
+      .then((res) => {
         console.log(res.data);
         setProducts(res.data);
+        setFilteredProducts(res.data);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/categories")
+      .then((res) => {
+        console.log(res.data);
+        setCategories(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (selectedCategory) {
+      axios
+        .get(`http://localhost:3000/api/products/categorie/${selectedCategory}`)
+        .then((res) => {
+          console.log(res.data);
+          setFilteredProducts(res.data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setFilteredProducts(products);
+    }
+  }, [selectedCategory, products]);
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
   return (
     <div>
       <Navbar />
-      
-      <Form.Group id="selectCategorie" className="mb-3">
-        <Form.Label style={{color:"white"}}>Disabled select menu</Form.Label>
-        <Form.Select disabled>
-          <option>Disabled select</option>
-        </Form.Select>
-      </Form.Group>
-      
-      <div id="cards" className="container" >
+      <Form>
+        <Form.Group id="selectCategorie" className="mb-3">
+          <Form.Label style={{ color: "white" }}>Select a category</Form.Label>
+          <Form.Select onChange={handleCategoryChange} value={selectedCategory}>
+            <option value="">All Categories</option>
+            {categories.map((categorie) => (
+              <option key={categorie.id} value={categorie.id}>
+                {categorie.name}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </Form>
+
+      <div id="cards" className="container">
         <div className="row">
-          {products.map(product => (
-            <div key={product.id} className="col-md-4" >
-              <div className="product-item" style={{backgroundColor:"white"}}>
-                <a href="#" target="_blank">
-                  <img src={"http://localhost:3000/assets/uploads/"+product.image} alt={product.image} />
-                </a>
-                <div className="down-content">
+          {filteredProducts.length === 0 ? (
+            <p style={{ color: "white" }}>Aucun produit trouvé !</p>
+          ) : (
+            filteredProducts.map((product) => (
+              <div key={product.id} className="col-md-4">
+                <div
+                  className="product-item"
+                  style={{ backgroundColor: "white" }}
+                >
                   <a href="#" target="_blank">
-                    <h4>{product.name}</h4>
+                    <img
+                      src={
+                        "http://localhost:3000/assets/uploads/" + product.image
+                      }
+                      alt={product.image}
+                    />
                   </a>
-                  <p id="descriptionCard">
-                    {product.description}
-                  </p>
-                  <button id="btnCard">Voir plus</button>
+                  <div className="down-content">
+                    <a href="#" target="_blank">
+                      <h4>{product.name}</h4>
+                    </a>
+                    <p id="descriptionCard">{product.description}</p>
+                    <button id="btnCard">Voir plus</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
       <Footer />
